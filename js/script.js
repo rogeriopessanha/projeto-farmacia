26044e7c9fe2b6cb0804d7c4115348dde6ec2fc6
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2022-09-20 23:27:59 -0300

    att

--- js/script.js
+++ js/script.js
@@ -6,16 +6,6 @@ document.querySelector('#botao-pesquisar').onclick =() =>{
 }
 
 
-// let loginForm = document.querySelector('.login-form-container');
-
-// document.querySelector('#login-btn').onclick = () => {
-//     loginForm.classList.toggle(active);
-// }
-
-// document.querySelector('#close-login-btn').onclick = () =>{
-//     loginForm.classList.remove(active);
-// }
-
 let loginForm = document.querySelector('.login-form-container');
 
 document.querySelector('#login-btn').onclick = () => {
@@ -37,5 +27,36 @@ window.onscroll = () => {
     } else {
         document.querySelector('..topo .topo-2').classList.remove('active')
     }
-
 }
+
+
+
+
+var swiper = new Swiper(".featured-slider", {
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: true,
+    grabCursor: 'true',
+    autoplay: {
+        delay: 6500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        450: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+    },
+});
