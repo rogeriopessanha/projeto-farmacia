b91aac41acec06dc9bd68e29f3eaaac5f5c53658
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2022-09-21 22:15:20 -0300

    att

--- js/script.js
+++ js/script.js
@@ -25,26 +25,62 @@ window.onscroll = () => {
     if (window.scrollY > 80) {
         document.querySelector('.topo .topo-2').classList.add('active')
     } else {
-        document.querySelector('..topo .topo-2').classList.remove('active')
+        document.querySelector('.topo .topo-2').classList.remove('active')
+    }
+}
+
+window.onload = () => {
+    if (window.scrollY > 80){
+        document.querySelector('.topo .topo-2').classList.add('active')
+    } else{
+        document.querySelector('.topo .topo-2').classList.remove('active')
     }
 }
 
 
 
 
-var swiper = new Swiper(".mySwiper", {
-    // spaceBetween: 30,
+var swiper = new Swiper(".banner-slider", {
+    spaceBetween: 20,
     centeredSlides: true,
     autoplay: {
-        delay: 10000,
+        delay: 5000,
         disableOnInteraction: false,
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
     // navigation: {
     //     nextEl: ".swiper-button-next",
     //     prevEl: ".swiper-button-prev",
     // },
+});
+
+
+var swiper = new Swiper(".produtos-slider", {
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: true,
+    // grabCursor: 'true',
+    autoplay: {
+        delay: 6500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        
+        450: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+    },
 });
